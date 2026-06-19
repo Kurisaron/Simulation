a8348ad687472f6075ec2558ffed016c88e720d6
Kurisaron <103210710+Kurisaron@users.noreply.github.com>
2026-06-19 06:23:45 -0700

    Restart

--- Simulation/Source/Simulation/Private/Trait.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Trait.h"
-
-UTrait::UTrait(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-
-}
